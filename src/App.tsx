06deb86f7379d106289ac23adef214a3b25efd1a
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-10-25 00:56:20 +0200

    Work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { events } from './calendar/tests/Event.fixtures';
 import EventList from './calendar/components/EventList';
+import { useRecoilValue } from 'recoil';
+import { GetEvents } from './calendar/state/event';
 
 function App() {
+  const events = useRecoilValue(GetEvents);
+  console.log(events);
+
   return <EventList events={events} />;
 }
 
