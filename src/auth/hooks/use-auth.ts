b942338914e1ac553776c1a5d4431c29897d174a
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-11-07 10:40:40 +0100

    Work

--- src/auth/hooks/use-auth.ts
+++ src/auth/hooks/use-auth.ts
@@ -1,14 +1,18 @@
+import { useRecoilState } from 'recoil';
+
 import { deleteAccessToken, setAccessToken } from '../service/tokens';
 import { organizationConfig } from '../../../config/organization';
 import { mainApi } from '../../lib/api';
 import { User } from '../types/user';
-import { useRecoilState } from 'recoil';
 import { CurrentUser } from '../state/user';
 
 export function useAuth() {
   const [user, setUser] = useRecoilState<User | undefined>(CurrentUser);
 
-  const signInUser = async (credentials: { email: string; password: string }) => {
+  const signInUser = async (credentials: {
+    email: string;
+    password: string;
+  }) => {
     const response = await mainApi.post(
       `/users/${organizationConfig.id}/login`,
       credentials,
