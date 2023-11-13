6f2e709d40e858912b3a233165ce13596379b16f
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-11-13 09:09:26 +0100

    feat: [sc-121] Website Footer (#21)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
-import { Col, Result, Row } from 'antd';
+import { Button, Col, Result, Row, Space } from 'antd';
 
 import MenuBar from './components/MenuBar';
 import FullPageSpin from './components/FullPageSpin';
 import AboutPage from './about/AboutPage';
-import { QuestionIcon } from './components/Icons';
+import {
+  FacebookIcon,
+  QuestionIcon,
+  TwitterIcon,
+  YouTubeIcon,
+} from './components/Icons';
 
 const HomePage = lazy(() => import('./home/HomePage'));
 const CalendarPage = lazy(() => import('./calendar/CalendarPage'));
@@ -18,35 +23,72 @@ function App() {
       <Col
         lg={20}
         xl={14}
-        style={{ minHeight: '100vh', backgroundColor: 'white' }}
+        style={{
+          minHeight: '100vh',
+          backgroundColor: 'white',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
       >
-        <Row style={{ padding: '32px 64px' }}>
-          <Col span={4}>
-            <Link to="/">
-              <img src="/logo.png" style={{ width: '100%' }} />
-            </Link>
-          </Col>
-        </Row>
-        <MenuBar />
-        <Suspense fallback={<FullPageSpin />}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/hjem" replace />} />
-            <Route path="/hjem" element={<HomePage />} />
-            <Route path="/kalender" element={<CalendarPage />} />
-            <Route path="/om-os" element={<AboutPage />} />
-            <Route path="/log-ind" element={<LoginPage />} />
-            <Route path="/registrer" element={<RegisterPage />} />
-            <Route
-              path="*"
-              element={
-                <Result
-                  icon={<QuestionIcon />}
-                  title="Denne side findes ikke"
-                />
-              }
-            />
-          </Routes>
-        </Suspense>
+        <header>
+          <Row style={{ padding: '32px 64px' }}>
+            <Col span={4}>
+              <Link to="/">
+                <img src="/logo.png" style={{ width: '100%' }} />
+              </Link>
+            </Col>
+          </Row>
+          <MenuBar />
+        </header>
+        <article style={{ flex: 1 }}>
+          <Suspense fallback={<FullPageSpin />}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/hjem" replace />} />
+              <Route path="/hjem" element={<HomePage />} />
+              <Route path="/kalender" element={<CalendarPage />} />
+              <Route path="/om-os" element={<AboutPage />} />
+              <Route path="/log-ind" element={<LoginPage />} />
+              <Route path="/registrer" element={<RegisterPage />} />
+              <Route
+                path="*"
+                element={
+                  <Result
+                    icon={<QuestionIcon />}
+                    title="Denne side findes ikke"
+                  />
+                }
+              />
+            </Routes>
+          </Suspense>
+        </article>
+        <footer
+          style={{
+            backgroundColor: '#E1E1E1',
+            width: '100%',
+            padding: 32,
+            boxSizing: 'border-box',
+          }}
+        >
+          <Row justify="space-between">
+            <Col>
+              Luthersk Missions Unge i Odense
+              <br />
+              Rosenlunden 15
+              <br />
+              5000 Odense C<br />
+              odense@lmu.dk
+              <br />
+              CVR-nr.: 30789334
+            </Col>
+            <Col>
+              <Space direction="vertical">
+                <Button icon={<FacebookIcon />} shape="circle" />
+                <Button icon={<TwitterIcon />} shape="circle" />
+                <Button icon={<YouTubeIcon />} shape="circle" />
+              </Space>
+            </Col>
+          </Row>
+        </footer>
       </Col>
     </Row>
   );
