59ea15b48127d9940f9ac72589b171fceb568168
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-11-15 12:24:48 +0100

    Work

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,16 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Spin } from 'antd';
 
 import App from './App.tsx';
 import AllProviders from './components/AllProviders.tsx';
+import FullPageSpin from './components/FullPageSpin.tsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AllProviders>
-      <Suspense fallback={<Spin />}>
+      <Suspense fallback={<FullPageSpin />}>
         <App />
       </Suspense>
     </AllProviders>
